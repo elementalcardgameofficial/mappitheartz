38ed9b7dc327a1c4c22a431addb655cf48c13b93
elementalcardgameofficial <elementalcardgameofficial@gmail.com>
2026-05-09 21:11:26 +0300

    Add files via upload

---
+++ auth-config.js
@@ -0,0 +1,4 @@
+window.MAPPIT_AUTH = {
+  supabaseUrl: "https://mxzwtwhirpnccerrijrb.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14end0d2hpcnBuY2NlcnJpanJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NTA2MTIsImV4cCI6MjA5MzEyNjYxMn0.HR9cM13Rxy2Af-KC4W0D2IXvDF-4DHeN0HxsJkBxHGI"
+};
